d995ea9a4daf198fbd60b442e713f7e5c2ca25de
Muchtar Salimov <muchtarsalimov@gmail.com>
2024-10-17 23:45:41 -0400

    finish exercise 3.4

--- index.js
+++ index.js
@@ -41,6 +41,15 @@ app.get('/api/persons/:id', (request, response) => {
   return response.json(person)
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const person = persons.find(entry=> entry.id === request.params.id)
+  if ( !person ) {
+    return response.status(204).send({ error: 'id not found' })
+  }
+  persons = persons.filter(entry=>entry.id !== request.params.id)
+  return response.status(200).json(person)
+})
+
 app.get('/info', (request, response) => {
   const date = new Date(Date.now())
   response.send(`Phonebook has info for ${ persons.length } people <br/> ${date.toString()}`)
