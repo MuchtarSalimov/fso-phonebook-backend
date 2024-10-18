65a0ff47ac27d8010848d519d4189111598ee1ba
Muchtar Salimov <muchtarsalimov@gmail.com>
2024-10-17 23:15:20 -0400

    finish exercise 3.1

--- index.js
+++ index.js
@@ -1,48 +1,35 @@
 const express = require('express')
 const app = express()
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
   },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
   },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
   }
 ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.json(persons)
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
 const PORT = 3001
