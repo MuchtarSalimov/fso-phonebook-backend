e4122ac78af1e7965b422e161ff2b00fb360b176
Muchtar Salimov <muchtarsalimov@gmail.com>
2024-10-17 23:26:02 -0400

    finish exercise 3.2

--- index.js
+++ index.js
@@ -25,11 +25,17 @@ let persons = [
 ]
 
 app.get('/', (request, response) => {
-  response.json(persons)
+  const date = new Date(Date.now())
+  response.send(`Phonebook has info for ${ persons.length } people <br/> ${date.toString()}`)
 })
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const date = new Date(Date.now())
+  response.send(`Phonebook has info for ${ persons.length } people <br/> ${date.toString()}`)
 })
 
 const PORT = 3001
